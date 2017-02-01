1ec42f5b89389472a13c5d24044b3a232a5a667c
tnecamp <tnecamp@gmail.com>
2017-02-01 01:30:00 -0500

    Update simulation_evaluate_coverage.py

--- simulation_evaluate_coverage.py
+++ simulation_evaluate_coverage.py
@@ -1,41 +1,125 @@
 # This should be run in Python 3
+
+## Double check carefulness about the seed
+## Things are done in the setting where I know the value of the likelihood of the MLE but I don't observe the MLE directly
+## I also only do estimation for one set of initial estimates, I could change how I initialize my quadratic, 
+## and how many initial points I use
+
+## double check gamma and normal pdfs
+## double check hessian inverse stuff
+
+
 import numpy as np
 from scipy.stats import multivariate_normal
+from scipy.stats import gamma
+from scipy.stats import norm
+from scipy.optimize import minimize
+### Create storage variables for each simulation ###
 
-			## Here I have to reset the seed because, if not, the seed reset in my optimization will mess up 
-			## my data
-			set.seed(k)
+## speed:Make vectors of length iter for each round so we can store all the estimates from each iteration and 
+## take summary statistics at the end
+true_L = []
+true_U = []
+true_MLE = []
+L_vec = []
+U_vec = []
+L_vec_noise = []
+U_vec_noise = []
+L_vec_g_noise = []
+U_vec_g_noise = []
+MLE_hat = []
+MLE_var = []
+a_store = []
+b_store = []
+a_reparam_store = []
+a_var_store = []
+b_var_store =  []
+a_reparam_var_store = []
 
+## These will be updated additively, could have stored as a vector and taken mean, but no need
+SD_g = 0
+bias_g = 0
 
-### Create data set for this iteration and true parameter values ###
+### Define optimization function to get estimate of profile likelihood ###
+
+## This is where I define the meta model optimization function, it finds the quadratice with the highest
+## likelihood for a give meta-model
+## a_init,b_init,c_reparam define the initial quadratic guess
+## x_star and y_star are the data values
+## y_star_max is the maximum of all y_star, helpful when parameterizing c, bu could remove
+## mcmc_sample_size is the number of monte carlo points you want to use to approximate the distribution
+## sample_size is how many data points I have to evaluate the likelihood at
+## This function works for any set of x_star, y_star but is particular to the specified distributions
+def meta_model_optimization(a_init, b_init, c_reparam, x_star, y_star, x_star_sd, y_star_max, mcmc_sample_size, sample_size):
+	## Define likelihood function to optimize
+	def full_log_likelihood_alpha(a, b, c_reparam):
+		## Set seed every time to make the mcmc sample consistent, i.e. for the same parameters, I will get the same likelihood
+		## estimate
+		np.random.seed(100)
+		mcmc_sample = np.random.normal(-b/(2*a), np.sqrt(-1/(2*a)), mcmc_sample_size)
+		temp  = a*np.square(mcmc_sample) + b*mcmc_sample + c_reparam + b**2/(4*a) + y_star_max
+		cur_sum = 0
+		  
+		## Should I vectorize this?
+		for j in range(sample_size):
+			like_y_star = gamma.pdf(temp - y_star[j], a = .5, scale = 1)
+			like_x_star = norm.pdf(mcmc_sample-x_star[j], loc = 0, scale = x_star_sd)
+			cur_sum = cur_sum + np.log(1/mcmc_sample_size*sum(like_y_star*like_x_star))
+		
+		return(cur_sum)
 	
-	## Simulation parameter
-sigma = 2*np.array([[1, .5], [.5, 3]])
-sigma_inv = np.linalg.inv(sigma)
-mu = np.array([-5.1, 5.2])
-n = 20
 
-## Create data
+	## Within this I reparameterize to allow the optimization to not have any constraints, also prevents 
+	## optimization errors
+	def optim_fun2(arg_vec):
+		a = -np.exp(arg_vec[0])
+		b = arg_vec[1]
+		return(-full_log_likelihood_alpha(a, b, c_reparam))
+	
+	
+	init_array = np.array([np.log(-a_init), b_init])
+	optim_sol = minimize(optim_fun2, x0 = init_array, method = 'BFGS')
+	return(optim_sol)
 
-data = np.random.multivariate_normal(mu, sigma, n)
-data_mean = np.mean(data, axis = 0)
 
-## Get the true profile likelihood function, need to redefine this everytime I get a new data set
-def pl_eval(theta):
-	## mu1 = theta case
-	mu_2_max = data_mean[1] + sigma_inv[0,1]/sigma_inv[1,1]*(data_mean[0]-theta)
-	mu_2_max = min(theta, mu_2_max)
-	like_1 = multivariate_normal.logpdf(np.array([theta, mu_2_max]), mean = data_mean, cov = sigma/n)
+                                 	
 
-	## mu2 = theta case
-	mu_1_max = data_mean[0] + sigma_inv[0,1]/sigma_inv[0,0]*(data_mean[1]-theta)
-	mu_1_max = min(theta, mu_1_max)
-	like_2 = multivariate_normal.logpdf(np.array([mu_1_max,theta]), mean = data_mean, cov = sigma/n)
-	return(max(like_1,like_2))
+### Begin Simulation ###
+coverage_iter_number = 5		# number of simulations I will use to assess the coverage               
+for k in range(coverage_iter_number):
+	## Here I have to reset the seed because, if not, the seed reset in my optimization will mess up 
+	## my data
+	np.random.seed(k)
+
+	### Create data set for this iteration and true parameter values ###
+		
+		## Simulation parameter
+	sigma = 2*np.array([[1, .5], [.5, 3]])
+	sigma_inv = np.linalg.inv(sigma)
+	mu = np.array([-5.1, 5.2])
+	n = 20
+
+	## Create data
+
+	data = np.random.multivariate_normal(mu, sigma, n)
+	data_mean = np.mean(data, axis = 0)
+
+	## Get the true profile likelihood function, need to redefine this everytime I get a new data set
+	def pl_eval(theta):
+		## mu1 = theta case
+		mu_2_max = data_mean[1] + sigma_inv[0,1]/sigma_inv[1,1]*(data_mean[0]-theta)
+		mu_2_max = min(theta, mu_2_max)
+		like_1 = multivariate_normal.logpdf(np.array([theta, mu_2_max]), mean = data_mean, cov = sigma/n)
+
+		## mu2 = theta case
+		mu_1_max = data_mean[0] + sigma_inv[0,1]/sigma_inv[0,0]*(data_mean[1]-theta)
+		mu_1_max = min(theta, mu_1_max)
+		like_2 = multivariate_normal.logpdf(np.array([mu_1_max,theta]), mean = data_mean, cov = sigma/n)
+		return(max(like_1,like_2))
 
 	## Getting MLE of my underlying data set
 	mle_sample = max(data_mean)
-	true_MLE[k] = mle_sample
+	true_MLE.append(mle_sample)
 
 	## Get the likelihood value of MLE
 	mle_like = multivariate_normal.logpdf(data_mean, mean = data_mean, cov = sigma/n)
@@ -51,8 +135,8 @@ def pl_eval(theta):
 	# true confidence interval lower and upper bound, i.e. if I knew the true profile likelihood
 	true_L_cur = theta_set[(theta_set_like > pl_cutoff)][0] -.0005	# lower bound
 	true_U_cur = theta_set[(theta_set_like > pl_cutoff)][-1] + .0005 	  # upper bound
-	true_L[k] = true_L_cur   # storage
-	true_U[k] = true_U_cur	 # storage
+	true_L.append(true_L_cur)   # storage
+	true_U.append(true_U_cur)	 # storage
 
 
 	### Generate points 'below' the profile likelihood that I will use to estimate true profile likelihood ###
@@ -92,30 +176,28 @@ def pl_eval(theta):
 	b_init = -2*curvature*center
 	c_reparam = height - y_star_max
 
-## here
 	## Find the optimized quadratice parameters, i.e. my PL estimate
 	optimized_parameters = meta_model_optimization(a_init, b_init, c_reparam, mu_hat_max, 
 		likehood_sample, x_star_sd, y_star_max, 10000, sample)
 	
-	a_reparam = optimized_parameters[[1]]
-	a = -exp(a_reparam)
-	b = optimized_parameters[[2]]
-	information_est = optimized_parameters[[3]]		## keep this positive since I minimized the negative log likelihood
-##
+	a_reparam = optimized_parameters.x[0]
+	a = -np.exp(a_reparam)
+	b = optimized_parameters.x[1]
+	information_inv_est = optimized_parameters.hess_inv	## keep this positive since I minimized the negative log likelihood
 
-### Storage of values and finding new cut offs for our profile likelihood ###
+	### Storage of values and finding new cut offs for our profile likelihood ###
 	
-	MLE_hat[k] = -b/(2*a)		# Store estimate of MLE based on PL
+	MLE_hat.append(-b/(2*a))		# Store estimate of MLE based on PL
 	grad_mle = np.array([-b/(2*np.exp(a_reparam)), 1/(2*np.exp(a_reparam))])
-	cur_inv = np.linalg.inv(information_est)		# get error estimates of parameters based on hessian
+	cur_inv = information_inv_est		# get error estimates of parameters based on hessian
 	cur_MLE_var = grad_mle.dot(cur_inv).dot(grad_mle)	# get estimate of MLE variance
-	MLE_var[k] = cur_MLE_var	# store MLE variance estimate
-	a_store[k] = a		# store curvaturue
-	b_store[k] = b		# store b value in quadratice
-	a_reparam_store[k] = a_reparam
-	a_var_store[k] = np.exp(-2*a_reparam)*cur_inv[0,0]		# variance in a estimate
-	b_var_store[k] =  cur_inv[1,1]		# variance in b estimate
-	a_reparam_var_store[k] = cur_inv[0,0]		# variance in reparameterized a
+	MLE_var.append(cur_MLE_var)	# store MLE variance estimate
+	a_store.append(a)		# store curvaturue
+	b_store.append(b)		# store b value in quadratice
+	a_reparam_store.append(a_reparam)
+	a_var_store.append(np.exp(-2*a_reparam)*cur_inv[0,0])	# variance in a estimate
+	b_var_store.append(cur_inv[1,1])	# variance in b estimate
+	a_reparam_var_store.append(cur_inv[0,0])		# variance in reparameterized a
 	
 	## obtain new profile likelihood cutoff based on estimated PL
 	new_cut_off = y_star_max - 1.92 - 3.84*(-a)*max(cur_MLE_var,0)  # Tim double check this should be y_star_max vs mle_like
@@ -124,9 +206,8 @@ def pl_eval(theta):
 	L_vec_noise_cur =  -np.sqrt((new_cut_off - (c_reparam+y_star_max)) / a ) - b/(2*a)		# New estimated lower bound
 	U_vec_noise_cur =  np.sqrt((new_cut_off - (c_reparam+y_star_max)) / a ) - b/(2*a)		# New estimated upper bound
 	
-	L_vec_noise[k] =  L_vec_noise_cur
-	U_vec_noise[k] =  U_vec_noise_cur 
-
+	L_vec_noise.append(L_vec_noise_cur)
+	U_vec_noise.append(U_vec_noise_cur) 
 
 
 	### Create a plots to check out the results while it's running ###
@@ -141,11 +222,11 @@ def pl_eval(theta):
 	#points(true_U_cur, pl_cutoff, col = 4)
 	
 	## Plot the initial estimate of the profile likelihood in turqouise
-	#quad_fun = function(x) {return(a_init*(x+b_init/(2*a_init))^2 + c_reparam + y_star_max)}
+	#quad_fun = function(x) {return(a_init*(x+b_init/(2*a_init))**2 + c_reparam + y_star_max)}
 	#points(theta_set, quad_fun(theta_set),col=5, cex = .02)
 	
 	## Plot the optimized PL estimate in pink
-	#quad_fun = function(x) {return(a*(x+b/(2*a))^2 + c_reparam+y_star_max)}
+	#quad_fun = function(x) {return(a*(x+b/(2*a))**2 + c_reparam+y_star_max)}
 	#points(theta_set, quad_fun(theta_set),col=6, cex = .02)
 	
 	## Plot the new cut off and lower and bounds for estimated PL
@@ -155,8 +236,7 @@ def pl_eval(theta):
 	#points(U_vec_noise_cur, new_cut_off, col = 4)
 	
 	## Print the iteration
-	#print(k)
-}
+	print(k)
 
 
 ### Check out results ###
